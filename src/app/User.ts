21dc672d27524623055bcf70a65c959882eb702d
ShreyaReddy-150 <110247732+ShreyaReddy-150@users.noreply.github.com>
2022-09-05 16:32:46 +0530

    Delete src directory

--- src/app/User.ts
+++
@@ -1,7 +0,0 @@
-export interface Users {
-    "name": string,
-    "email": string,
-    "phNo": number,
-    "pwd": string,
-    "loggedIn": null
-}
--- src/app/bookInterface.ts
+++
@@ -1,9 +0,0 @@
-export interface Books {
-    "id": number,
-    "bname": string,
-    "aname": string,
-    "pdate": string,
-    "pname": string,
-    "lang": string,
-    "bimg": string
-}
